1f34202bb8df51dc32e3539c7401b041c326cc48
Andrei Shim <art.shim@yandex.ru>
2023-11-06 14:24:56 +0300

    Исправил сохранение текущих результатов в классе Розыгрыша, добавил метод загрузки результатов из файла

--- src/main/java/model/Controller.java
+++ src/main/java/model/Controller.java
@@ -19,53 +19,54 @@ public class Controller<T extends Toy> {
         this.inventory = new Inventory<>();
     }
 
-    public void addToy(int id, int chance, String name){
+    public void addToy(int id, int chance, String name) {
         this.inventory.putToy((T) new Toy(id, name, chance));
     }
 
     public T draw() throws NotEnoughToys {
-        this.drawing = new ToyDrawing<>(inventory.getToys());
+        if (this.drawing == null) {
+            this.drawing = new ToyDrawing<>(inventory.getToys());
+        }
         this.drawing.setDrawing();
         return drawing.getToy();
     }
 
-    public String showInventory(){
+    public String showInventory() {
         if (inventory.toString().isBlank()) return null;
         return inventory.toString();
     }
 
-    public boolean clearInventory(){
+    public boolean clearInventory() {
         if (inventory.toString().isBlank()) return false;
         inventory.clear();
         return true;
     }
 
-    public boolean toyInInventory(int id){
+    public boolean toyInInventory(int id) {
         return inventory.contains(id);
     }
 
     public void correctToy(int correctID, String correctName, int correctChance) throws WrongIDException {
-        inventory.correctToy(correctID, (T) new Toy(correctID,correctName,correctChance));
+        inventory.correctToy(correctID, (T) new Toy(correctID, correctName, correctChance));
     }
 
     public String showResults() throws DrawResultIsEmpty {
-        if(drawing == null) throw new DrawResultIsEmpty();
+        if (drawing == null || drawing.getResults() == null) throw new DrawResultIsEmpty();
         return drawing.getResults();
     }
 
     public void saveResults() throws DrawResultIsEmpty {
-        if(drawing == null || drawing.getResults().isBlank()) throw new DrawResultIsEmpty();
+        if (drawing == null || drawing.getResults().isBlank()) throw new DrawResultIsEmpty();
         exporter.saveToFile(drawing.getResults());
         drawing.clearResults();
     }
 
     public void deleteResults() throws DrawResultIsEmpty {
-        if(drawing == null || drawing.getResults().isBlank()) throw new DrawResultIsEmpty();
+        if (drawing == null || drawing.getResults().isBlank()) throw new DrawResultIsEmpty();
         drawing.clearResults();
     }
 
-    public void clearFile() throws DrawResultIsEmpty {
-        if(drawing == null || drawing.getResults().isBlank()) throw new DrawResultIsEmpty();
+    public void clearFile() {
         exporter.clearFile();
     }
 }
--- src/main/java/model/Exporter.java
+++ src/main/java/model/Exporter.java
@@ -1,13 +1,33 @@
 package model;
 
+import java.io.*;
+
 public class Exporter {
-    public void saveToFile(String results) {
-        //TODO
-        System.out.println("!!! Exporter.saveToFile is not done!!!");
+    public void saveToFile(String results) throws RuntimeException {
+        try (FileWriter fw = new FileWriter("winners.csv", true)) {
+            fw.write(results + '\n');
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void clearFile() {
-        //TODO
-        System.out.println("!!! Exporter.clearFile is not done!!!");
+    public void clearFile() throws RuntimeException {
+        try (FileWriter fw = new FileWriter("winners.csv")) {
+            fw.write("");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String loadFile() throws RuntimeException{
+        String result = "";
+        try(BufferedReader br = new BufferedReader(new FileReader("winners.csv"))){
+            while (br.ready()){
+                result += br.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 }
